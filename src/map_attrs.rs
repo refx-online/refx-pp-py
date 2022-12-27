f3c4ea9cb8e32bf5f858a515f47404e0f8270088
James <tsunyoku@gmail.com>
2022-12-27 22:38:18 +0000

    feat: update map attr imports

--- src/map_attrs.rs
+++ src/map_attrs.rs
@@ -1,7 +1,7 @@
 use std::fmt::{Display, Formatter, Result as FmtResult};
 
 use pyo3::{pyclass, pymethods};
-use rosu_pp::{beatmap::BeatmapAttributes, Beatmap};
+use akatsuki_pp::{beatmap::BeatmapAttributes, Beatmap};
 
 #[pyclass(name = "BeatmapAttributes")]
 pub struct PyBeatmapAttributes {
